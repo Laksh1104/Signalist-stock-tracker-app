5691f6a129245886d4316f04417d8070ff4755b7
Laksh1104 <laksh.boodnah@mail.utoronto.ca>
2025-12-27 13:54:54 -0500

    Made suggested changed from code review #4

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -2,6 +2,10 @@ import { connectToDatabase } from "@/database/mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    if (process.env.NODE_ENV === 'production') {
+              return NextResponse.json({ message: "Endpoint not available" }, { status: 404 });
+         }
+
     try {
         await connectToDatabase();
         return NextResponse.json({ message: "Connected to database successfully!" }, { status: 200 });
