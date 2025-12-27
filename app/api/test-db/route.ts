830a44243f02fb41e46b4fd89ae8d2cdea2dbd1d
Laksh1104 <laksh.boodnah@mail.utoronto.ca>
2025-12-27 13:48:09 -0500

    Implemented database setup

---
+++ app/api/test-db/route.ts
@@ -0,0 +1,11 @@
+import { connectToDatabase } from "@/database/mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectToDatabase();
+        return NextResponse.json({ message: "Connected to database successfully!" }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ message: "Failed to connect to database", error: error.message }, { status: 500 });
+    }
+}
